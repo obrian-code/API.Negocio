2b6f627598a6d273c39f3b695ad04ecb49f1974f
obrian-code <lgjhon@gmail.com>
2022-08-15 12:49:39 -0500

    v1.0

---
+++ database/negocio_detallefactura.sql
@@ -0,0 +1,56 @@
+-- MySQL dump 10.13  Distrib 8.0.29, for Win64 (x86_64)
+--
+-- Host: localhost    Database: negocio
+-- ------------------------------------------------------
+-- Server version	8.0.29
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `detallefactura`
+--
+
+DROP TABLE IF EXISTS `detallefactura`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `detallefactura` (
+  `codigo` int NOT NULL AUTO_INCREMENT,
+  `codigoFactura` int DEFAULT NULL,
+  `nombreProducto` varchar(45) DEFAULT NULL,
+  `cantidad` int DEFAULT NULL,
+  `precioUnidad` double DEFAULT NULL,
+  PRIMARY KEY (`codigo`),
+  KEY `codigoFactura` (`codigoFactura`),
+  CONSTRAINT `detallefactura_ibfk_1` FOREIGN KEY (`codigoFactura`) REFERENCES `factura` (`codigo`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `detallefactura`
+--
+
+LOCK TABLES `detallefactura` WRITE;
+/*!40000 ALTER TABLE `detallefactura` DISABLE KEYS */;
+INSERT INTO `detallefactura` VALUES (1,1,NULL,2,520),(2,1,'Llantas ',1,351),(3,2,'Cemento',5,870.9),(4,3,'Clavos',1,20),(5,4,'Clavos',1,20);
+/*!40000 ALTER TABLE `detallefactura` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-08-15 12:46:59
